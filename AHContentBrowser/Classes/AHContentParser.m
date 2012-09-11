c2bba27e09e4bb1d76c1c9e63ce4714f95387f84
John Wright <mrjjwright@gmail.com>
2012-09-11 17:09:57 -0600

    use an array based approach

--- AHContentBrowser/Classes/AHContentParser.m
+++ AHContentBrowser/Classes/AHContentParser.m
@@ -9,7 +9,7 @@
 #import "AHContentParser.h"
 
 
-#define kAHContentStripUnwantedTags @"</?(?i:br|strong|ul|a|img)(.|\n)*?>"
+#define kAHContentTagsToKeep @"</?(?i:br|strong|ul|a|img|p)(.|\n)*?>"
 
 @interface AHContentTextChunk : NSObject
 
@@ -95,61 +95,36 @@
     // We don't parse things into a dom or html because it takes too long, and is really unreliable
     
     // Step 1: First look for paragraphs with lots of text, the quickest way to content on the web
-    NSRange pRange = [_htmlString rangeOfString:@"<p" options:NSCaseInsensitiveSearch];
-    NSRange endOfPRange = NSMakeRange(pRange.location + 2, pRange.length);
-    NSRange closingStartTagRange;
     AHContentTextChunk *currentChunk;
     //get paragraphs
-    while (pRange.location != NSNotFound) {
-        
-        endOfPRange = NSMakeRange(pRange.location + 2, pRange.length);;
-        BOOL nestedP = YES;
-        while (nestedP) {
-            NSRange nextPRange =  [_htmlString rangeOfString:@"<p" options:NSCaseInsensitiveSearch range:NSMakeRange(endOfPRange.location, _htmlString.length - endOfPRange.location)];
-            endOfPRange = [_htmlString rangeOfString:@"</p>" options:NSCaseInsensitiveSearch range:NSMakeRange(nextPRange.location, _htmlString.length - nextPRange.location)];
-            nestedP =  nextPRange.location < endOfPRange.location;
-        }
-        
-        if (endOfPRange.location != NSNotFound) {
-            closingStartTagRange = [_htmlString rangeOfString:@">" options:NSCaseInsensitiveSearch range:NSMakeRange(pRange.location + pRange.length, _htmlString.length - endOfPRange.location)];
+    
+    //_htmlString = [AHContentParser stringByStrippingHTML:_htmlString excludeRegEx:kAHContentTagsToKeep];
+    //NSUInteger *num = [AHContentParser countString:@"</p>" inText:_htmlString];
+    
+   NSArray * lines = [_htmlString componentsSeparatedByCharactersInSet: [NSCharacterSet characterSetWithCharactersInString:@"<>"]];
+    for (int i =0; i< lines.count; i++) {
+        NSRange pRange = [lines[i] rangeOfString:@"p" options:NSCaseInsensitiveSearch];
+        if (pRange.location ==0  ) {
             
+            // not a paragraph delimiter
+            if ([lines[i] length] > 1 && [lines[i] rangeOfString:@"=\""].location == NSNotFound) continue;
             
-            if (closingStartTagRange.location != NSNotFound) {
-                AHContentTextChunk *contentChunk = [[AHContentTextChunk alloc] init];
-                NSRange textRange = NSMakeRange(closingStartTagRange.location+1, endOfPRange.location - closingStartTagRange.location -1);
-                NSString *text =[_htmlString substringWithRange:textRange];
-                
-                // todo: score text
-                
-                text = [AHContentParser stringByStrippingHTML:text excludeRegEx:kAHContentStripUnwantedTags];
-                contentChunk.text = text;
-                contentChunk.textRange = textRange;
-                [_contentChunks addObject:contentChunk];
-                if (currentChunk) {
-                    contentChunk.previousChunk = currentChunk;
-                    currentChunk.nextChunk = contentChunk;
-                }
-                currentChunk = contentChunk;
-                
-                self.foundContent = YES;
-                pRange = [_htmlString rangeOfString:@"<p" options:NSCaseInsensitiveSearch range:NSMakeRange(endOfPRange.location, _htmlString.length -endOfPRange.location-1)];
+            NSString *text = lines[i+1];
+            text = [AHContentParser stringByStrippingHTML:text excludeRegEx:kAHContentTagsToKeep];
+            AHContentTextChunk *contentChunk = [[AHContentTextChunk alloc] init];
+            contentChunk.text = text;
+            [_contentChunks addObject:contentChunk];
+            if (currentChunk) {
+                contentChunk.previousChunk = currentChunk;
+                currentChunk.nextChunk = contentChunk;
             }
+            currentChunk = contentChunk;
+            
+            self.foundContent = YES;
+            
         }
     }
-    
-    
-    
-    // Look at the text in between our content to tell what we should exclude
-    NSUInteger maxDistance = 0;
-    for (AHContentTextChunk *chunk in _contentChunks) {
-        if (chunk.nextChunk) {
-            NSString *text = chunk.text;
-            NSString *nextText = chunk.nextChunk.text;
-            NSUInteger distanceBetweenChunks = chunk.nextChunk.textRange.location  - chunk.textRange.location + chunk.textRange.length;
-            maxDistance = MAX(maxDistance, distanceBetweenChunks);
-        }
-    }
-    
+      
     NSLog(@"Time to parse content: %f", [[NSDate date] timeIntervalSinceDate:startTime] );
     
 }
@@ -214,12 +189,34 @@
                 }
                 searchRange =  NSMakeRange(r.location, outString.length - r.location);
             }
+            
+            while ((r = [outString rangeOfString:@"<!--.*-->" options:NSRegularExpressionSearch]).location != NSNotFound)
+            {
+                    [outString deleteCharactersInRange:r];
+            }
+
         }
     }
     
-    NSLog(@"Time stringByStrippingHTMLFromString: %f", [[NSDate date] timeIntervalSinceDate:startTime] );
+    //NSLog(@"Time stringByStrippingHTMLFromString: %f", [[NSDate date] timeIntervalSinceDate:startTime] );
     return outString;
 }
 
++ (int)countString:(NSString *)stringToCount inText:(NSString *)text{
+     NSDate *startTime = [NSDate date];
+    
+    
+    int foundCount=0;
+    NSRange range = NSMakeRange(0, text.length);
+    range = [text rangeOfString:stringToCount options:NSCaseInsensitiveSearch range:range locale:nil];
+    while (range.location != NSNotFound) {
+        foundCount++;
+        range = NSMakeRange(range.location+range.length, text.length-(range.location+range.length));
+        range = [text rangeOfString:stringToCount options:NSCaseInsensitiveSearch range:range locale:nil];
+    }
+    
+    NSLog(@"Time countString: %f; found: %d", [[NSDate date] timeIntervalSinceDate:startTime], foundCount );
+    return foundCount;
+}
 
 @end
